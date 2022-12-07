b8f5f1d4d0fb38d175ed81c011e0c5ab2e495f92
goZabel <guilhermewill92@gmail.com>
2022-12-07 15:26:45 -0300

    Adição do ScrollReveal

---
+++ ScrollReveal.js
@@ -0,0 +1,38 @@
+window.rs = ScrollReveal({ reset: true });
+
+rs.reveal(".title-l", {
+  duration: 1000,
+  scale: 1.5,
+});
+
+rs.reveal(".btn-novidade", {
+  duration: 500,
+});
+
+rs.reveal(".recarga-title", {
+  duration: 500,
+  distance: "50px",
+});
+
+rs.reveal(".btn-recarga", {
+  duration: 500,
+});
+
+rs.reveal(".miniBanner", {
+  duration: 500,
+  distance: "50px",
+});
+
+rs.reveal(".card-cidade", {
+  duration: 1500,
+  distance: "50px",
+});
+
+rs.reveal(".sobre", {
+  duration: 1500,
+  rotate: {
+    x: 50,
+    y: 10,
+    z: 0,
+  },
+});
